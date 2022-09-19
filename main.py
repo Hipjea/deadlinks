d6398601f8d122377f1595775e024d598d1f6725
Hipjea <pierre.duverneix@gmail.com>
2022-09-19 12:22:58 +0200

    parse glob

--- main.py
+++ main.py
@@ -1,30 +1,44 @@
 import csv
+import glob
 import requests
 import yaml
+import os
+from dotenv import load_dotenv
 
 
-filepath = 'parcours-hybridation/arts-lettres-et-langues/36-licence-mention-humanites.yml'
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+filepath = os.getenv('DIRECTORY')
 csvfile = open('results.csv', 'w')
 header = ['url', 'status']
 
+# Write the CSV headers
 writer = csv.writer(csvfile)
 writer.writerow(header)
 
-with open(filepath, 'r') as f:
-    doc = yaml.safe_load(f)
+# Parse the YAML files
+for file in glob.glob(f'{filepath}/**/*.yml', recursive=True):
+    with open(file, 'r') as f:
+        doc = yaml.safe_load(f)
 
-    for element in doc["years"] :
-        for ue in element["ue"] :
-            for resource in ue["resources"] :
-                try:
-                    r = requests.get(resource["url"])
-                    if r.status_code != 200:
-                        writer.writerow([resource["url"], r.status_code])
-                except requests.ConnectionError:
-                    print("failed to connect : ", resource["url"])
-                    try:
-                        r = requests.get(resource["url"], verify=False)
-                        if r.status_code != 200:
-                            writer.writerow([resource["url"], r.status_code])
-                    except requests.ConnectionError:
-                        writer.writerow([resource["url"], r.status_code])
+        if doc.get('years'):
+            try:
+                for element in doc["years"]:
+                    for ue in element["ue"]:
+                        for resource in ue["resources"]:
+                            try:
+                                r = requests.get(resource["url"])
+                                if r.status_code != 200:
+                                    writer.writerow([resource["url"], r.status_code])
+                            except requests.ConnectionError:
+                                print("failed to connect : ", resource["url"])
+                                try:
+                                    r = requests.get(resource["url"], verify=False)
+                                    if r.status_code != 200:
+                                        writer.writerow([resource["url"], r.status_code])
+                                except requests.ConnectionError:
+                                    writer.writerow([resource["url"], r.status_code])
+            except BaseException as err:
+                print(f"Unexpected {err=}, {type(err)=}")
+                raise
