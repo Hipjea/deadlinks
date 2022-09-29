98fcf37d575c25103ab12c4ea49d1c4492cfda50
Hipjea <pierre.duverneix@gmail.com>
2022-09-29 14:43:34 +0200

    mailing

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ import glob
 import requests
 import yaml
 import os
+import smtplib
 from dotenv import load_dotenv
+from email.mime.application import MIMEApplication
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
@@ -11,10 +15,20 @@ load_dotenv(dotenv_path)
 
 TIMEOUT_DELAY = 20
 
+def has_smtp():
+    if smtp_user and smtp_pass:
+        return True
+    return False
+
+
+filename = 'results.csv'
 filepath = os.getenv('DIRECTORY')
-csvfile = open('results.csv', 'w')
+csvfile = open(filename, 'w')
 header = ['url', 'status']
 unique_urls = []
+
+smtp_user = os.getenv('SMTP_USER')
+smtp_pass = os.getenv('SMTP_PASS')
 
 # Write the CSV headers
 writer = csv.writer(csvfile)
@@ -62,3 +76,27 @@ for file in glob.glob(f'{filepath}/**/*.yml', recursive=True):
 
 csvfile.close()
 print("nombre d'URL :", len(unique_urls))
+
+if has_smtp():
+    # Send the infos by email
+    smtp_host = os.getenv('SMTP_HOST')
+    smtp_port = os.getenv('SMTP_PORT')
+    sender = os.getenv('SENDER')
+    receiver = os.getenv('RECEIVER')
+
+    with smtplib.SMTP(smtp_host, smtp_port) as server:
+        text = 'Détection des liens morts'
+        message = MIMEMultipart()
+        message['Subject'] = text
+        message['From'] = sender
+        message['To'] = receiver
+        message.attach(MIMEText(text))
+
+        with open(filename, "rb") as file:
+            part = MIMEApplication(file.read(), Name=os.basename(file))
+        part['Content-Disposition'] = 'attachment; filename="%s"' % os.basename(file)
+        message.attach(part)
+
+        server.login(smtp_user, smtp_pass)
+        server.sendmail(sender, receiver, message.as_string())
+        server.close()
